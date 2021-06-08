15d9522aac103f4ec05087180d4e6d238e3a2fa7
Andre Akira <andre.hayashi@usp.br>
2021-06-08 15:04:42 -0300

    Update Program.cs
    main

--- 02-Instrucoes/Program.cs
+++ 02-Instrucoes/Program.cs
@@ -4,6 +4,10 @@ namespace Instrucoes
 {
     class Program
     {
+        static void Main()
+        {
+            Console.WriteLine();
+        } 
         static void Declaracoes()
         {
             int a;
