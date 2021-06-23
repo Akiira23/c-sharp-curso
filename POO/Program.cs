d7018cf20c58213c326551a2af23b453a15bdcb5
Andre Akira <andre.hayashi@usp.br>
2021-06-23 18:48:30 -0300

    POO
    POO testes

--- POO/Program.cs
+++ POO/Program.cs
@@ -6,7 +6,13 @@ namespace POO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Produto p1 = new Produto();
+            p1.Codigo = 1;
+            p1.Nome = "Armario";
+            p1.Preco = 199.99;
+            Console.WriteLine($"cod: {p1.getCodigo()}");
+            Console.WriteLine($"nome: {p1.getNome()}");
+            Console.WriteLine($"preco: {p1.getPreco()}");
         }
     }
 }
--- POO/produto.cs
+++ POO/produto.cs
@@ -1,8 +1,25 @@
-public class Produto {
-    private int codigo;
-    private string nome;
-    private decimal preco;
-    public int Codigo {get => codigo; set => codigo = value;}
-    public string Nome {get => nome; set => nome = value;}
-    public decimal Preco {get => preco; set => preco = value;}
+namespace POO
+{
+    public class Produto 
+    {
+        private int codigo;
+        private string nome;
+        private double preco;
+        public int Codigo {set => codigo = value;}
+        public string Nome {set => nome = value;}
+        public double Preco {set => preco = value;}
+
+        public int getCodigo()
+        {
+            return this.codigo;
+        }
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public double getPreco()
+        {
+            return this.preco;
+        }
+    }
 }
