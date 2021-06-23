e088ee7c16c78dcae01da8593c1496402d5948bc
Andre Akira <andre.hayashi@usp.br>
2021-06-23 09:37:35 -0300

    desafios C#
    bazinga e selecao 1

---
+++ Solucionando desafios em C#/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Solucionando_desafios_em_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
