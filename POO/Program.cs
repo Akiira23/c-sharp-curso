0702dccaef949c9d7e9c438a12322985bb5f41a3
Andre Akira <andre.hayashi@usp.br>
2021-06-24 13:31:09 -0300

    POO alteracao
    array de classe

--- POO/Program.cs
+++ POO/Program.cs
@@ -6,13 +6,23 @@ namespace POO
     {
         static void Main(string[] args)
         {
-            Produto p1 = new Produto();
-            p1.Codigo = 1;
-            p1.Nome = "Armario";
-            p1.Preco = 199.99;
-            Console.WriteLine($"cod: {p1.getCodigo()}");
-            Console.WriteLine($"nome: {p1.getNome()}");
-            Console.WriteLine($"preco: {p1.getPreco()}");
+            Produto[] p1 = new Produto[2];
+            p1[0] = new Produto();
+            p1[0].Codigo = 1;
+            p1[0].Nome = "Armario";
+            p1[0].Preco = 199.99;
+            p1[1] = new Produto();
+            p1[1].Codigo = 2;
+            p1[1].Nome = "Sofa";
+            p1[1].Preco = 599.99;
+            foreach (Produto prod in p1)
+            {
+                Console.WriteLine($"cod: {prod.getCodigo()}");
+                Console.WriteLine($"nome: {prod.getNome()}");
+                Console.WriteLine($"preco: {prod.getPreco()}");
+                Console.WriteLine($"-------------------------");
+            }
+            
         }
     }
 }
